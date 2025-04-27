fa1483e39727ed12840a3df2147e0f3421ea6920
norjwall2 <137781707+norjwall2@users.noreply.github.com>
2025-04-27 16:49:49 +0800

    Update home.js
    add api key

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'd242ee6286bdfb503554a819442e9c68';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
